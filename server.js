d841bc98870d1e6797b74c6c0ffeb94a792ac945
sarthakadhikari1 <professionalsarthak@gmail.com>
2017-01-13 13:41:58 +0000

    First Message

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+app.get('/',function(req,res){
+    var ip = req.headers['x-forwarded-for'];
+    var language = req.headers["accept-language"].split(',')[0];
+    var userAgent =  req.headers['user-agent'];
+   res.json({'ip':ip,'language':language,'software':  userAgent.match(/\(.*?\)/)[0]});
+    
+})
+app.listen(8080);
